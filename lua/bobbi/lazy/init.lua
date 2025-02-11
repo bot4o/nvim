4f5c0745e40f727e67c578bfdf3ca62e239adbdd
bot4o <boevaleksandar@gmail.com>
2025-02-12 01:31:25 +0200

    changed to lazy

---
+++ lua/bobbi/lazy/init.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "nvim-lua/plenary.nvim",
+        name = plenary
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons'},
+        requires = { 'nvim-tree/nvim-web-devicons'}
+    },
+
+    -- "theprimeagen/refactoring.nvim",
+    -- 'jlcrochet/vim-razor',
+    -- 'Hoffs/omnisharp-extended-lsp.nvim',
+    -- 'BurntSushi/ripgrep',
+}
